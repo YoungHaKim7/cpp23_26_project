ea672cf0746950522089075173477c870203ae37
GYoung <ytok1108@aol.com>
2025-08-01 18:59:20 +0900

    https://en.cppreference.com/w/cpp/utility/functional/move_only_function.html

---
+++ cpp26_training/001_cpp26_tutorial/a06_std_move_only_function/README.md
@@ -0,0 +1,6 @@
+# Result
+- https://en.cppreference.com/w/cpp/utility/functional/move_only_function.html
+
+```bash
+Moved!
+```
---
+++ cpp26_training/001_cpp26_tutorial/a06_std_move_only_function/src/main.cpp
@@ -0,0 +1,9 @@
+// https://en.cppreference.com/w/cpp/utility/functional/move_only_function.html
+
+#include <functional>
+#include <print>
+
+int main() {
+    std::move_only_function<void()> f = [] { std::println("Moved!"); };
+    f();
+}
