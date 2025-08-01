466072432dc29615e91df0052e7d8acfcee4d984
GYoung <ytok1108@aol.com>
2025-08-01 18:48:27 +0900

    macOSㅇㅔ서는 에러남 LinuxOS하자

---
+++ cpp26_training/001_cpp26_tutorial/a05_assum_cond/README.md
@@ -0,0 +1,9 @@
+# Result
+
+```bash
+./src/main.cpp:3:10: fatal error: mdspan: No such file or directory
+    3 | #include <mdspan>
+      |          ^~~~~~~~
+compilation terminated.
+error: Recipe `r` failed on line 27 with exit code 1
+```
---
+++ cpp26_training/001_cpp26_tutorial/a05_assum_cond/src/main.cpp
@@ -0,0 +1,54 @@
+// https://en.cppreference.com/w/cpp/language/attributes/assume.html
+#include <cmath>
+#include <print>
+
+int divide(int a, int b) {
+  [[assume(b != 0)]];
+  return a / b;
+}
+
+void f(int &x, int y) {
+  void g(int);
+  void h();
+
+  [[assume(x > 0)]]; // Compiler may assume x is positive
+
+  g(x / 2); // More efficient code possibly generated
+
+  x = 3;
+  int z = x;
+
+  [[assume(
+      (h(), x == z))]]; // Compiler may assume x would have the same value after
+                        // calling h
+                        // The assumption does not cause a call to h
+
+  h();
+  g(x); // Compiler may replace this with g(3);
+
+  h();
+  g(x); // Compiler may NOT replace this with g(3);
+        // An assumption applies only at the point where it appears
+
+  z = std::abs(y);
+
+  [[assume((g(z), true))]]; // Compiler may assume g(z) will return
+
+  g(z); // Due to above and below assumptions, compiler may replace this with
+        // g(10);
+
+  [[assume(y == -10)]]; // Undefined behavior if y != -10 at this point
+
+  [[assume((x - 1) * 3 == 12)]];
+
+  g(x); // Compiler may replace this with g(5);
+}
+
+int main() {
+  auto res = divide(10, 2);
+  std::println("Result: ", res);
+  // compile error
+  // auto res2 = divide(10, 0);
+  // compile error
+  // std::println("Result: ", res);
+}
