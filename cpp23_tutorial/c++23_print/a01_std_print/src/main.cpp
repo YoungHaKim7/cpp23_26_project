cd7bdd7138a04b70f0dfc2dda4f7a1d3bde6bab0
GYoung <ytok1108@aol.com>
2025-08-02 15:42:21 +0900

    c++23 print

--- cpp23_tutorial/c++23_print/a01_std_print/src/main.cpp
+++ cpp23_tutorial/c++23_print/a01_std_print/src/main.cpp
@@ -1,15 +1,15 @@
+// https://en.cppreference.com/w/cpp/io/print.html
 #include <cstdio>
 #include <filesystem>
 #include <print>
- 
+
 int main()
 {
-    std::print("{0} {2}{1}!\n", "Hello", 23, "C++");  // overload (1)
- 
-    const auto tmp {std::filesystem::temp_directory_path() / "test.txt"};
- 
-    if (std::FILE* stream{std::fopen(tmp.c_str(), "w")})
-    {
+    std::print("{0} {2}{1}!\n", "Hello", 23, "C++"); // overload (1)
+
+    const auto tmp { std::filesystem::temp_directory_path() / "test.txt" };
+
+    if (std::FILE * stream { std::fopen(tmp.c_str(), "w") }) {
         std::print(stream, "File: {}", tmp.string()); // overload (2)
         std::fclose(stream);
     }
