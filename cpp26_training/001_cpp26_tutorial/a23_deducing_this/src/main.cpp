70101627978bf8b5d32e868f7232e7440500f1a6
GYoung <ytok1108@aol.com>
2025-08-03 23:57:00 +0900

    struct Point 출력하는 방법

---
+++ cpp26_training/001_cpp26_tutorial/a23_deducing_this/src/main.cpp
@@ -0,0 +1,27 @@
+#include <print>
+#include <vector>
+#include <format>
+
+struct Point {
+    int x, y;
+    auto move(this Point self, int dx, int dy) {
+        return Point{self.x + dx, self.y + dy};
+    }
+};
+
+template <>
+struct std::formatter<Point> {
+    constexpr auto parse(std::format_parse_context& ctx) {
+        return ctx.begin();
+    }
+
+    auto format(const Point& p, std::format_context& ctx) const {
+        return std::format_to(ctx.out(), "({}, {})", p.x, p.y);
+    }
+};
+
+int main()
+{
+    Point p{1, 2};
+    std::println("{}", p);
+}
